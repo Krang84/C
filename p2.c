3a1370a519f17d40d6719689982676fb3fd64c22
Krang84 <patelassad@gmail.com>
2026-02-03 16:17:55 +0100

    Create p2.c

---
+++ p2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int* ptr2;
+    //int *ptr = &x;
+    printf("%p \n", ptr2);
+    // $ gcc p2.c -o p2
+   // $ ./p2
+   // 0x7ffeb64e0aa0 
+    
+
+    
+}
